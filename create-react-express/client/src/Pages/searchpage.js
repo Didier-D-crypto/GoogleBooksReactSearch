83bea4d66c28057f963bec284365482bee5f78c1
dalex <delphindidier0@gmail.com>
2020-04-21 12:44:33 -0400

    searchpage

---
+++ create-react-express/client/src/Pages/searchpage.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+function Searchpage() {
+    const [searchTerm, setTerm]= useState('');
+    function searchFunction (e) {e.preventDefault(); console.log (searchTerm) }
+
+
+    return (
+        <div>
+            <p>Hello World</p>
+            <button onClick = {searchFunction}> Search Books </button>
+            <input onChange={e => {setTerm(e.target.value)}}></input>
+            
+
+    
+        </div>
+    )
+}
+
+export default Searchpage;
