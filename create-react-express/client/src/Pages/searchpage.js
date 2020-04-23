6a64a5ecb57df3ac3eabeb265ad22965a632e334
dalex <delphindidier0@gmail.com>
2020-04-23 12:31:40 -0400

    installed axios, setting up routes

--- create-react-express/client/src/Pages/searchpage.js
+++ create-react-express/client/src/Pages/searchpage.js
@@ -1,15 +1,37 @@
 import React, {useState} from 'react';
-
+import axios from 'axios'; 
 function Searchpage() {
     const [searchTerm, setTerm]= useState('');
     function searchFunction (e) {e.preventDefault(); console.log (searchTerm) }
+ const searchBooks = term => {
+     return axios.get("https://www.googleapis.com/books/v1/volumes", {
+         params:{
+             q: term
+         }
+     })
+ }
 
+ function handleSubmit( event ) {
+     event.preventDefault()
+     searchBooks(searchTerm)
+     .then (  //async portion
+         (
+             {
+                 data: {items: booklist}
+             }
+         ) => {
+            console.log()
+
+         }
+     )
+
+ }
 
     return (
         <div>
             <p>Google Books Search</p>
             <p>Enter the searched item below:</p>
-            <button onClick = {searchFunction}> Search Enumeration </button>
+            <button onClick = {handleSubmit}> Search Enumeration </button>
             <input onChange={e => {setTerm(e.target.value)}}></input>
              
             
