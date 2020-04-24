a7c7e6e37e218477cd3a938a00f56c12414b63b4
dalex <delphindidier0@gmail.com>
2020-04-24 03:45:32 -0400

    Updated forms within components along with stylesheets

---
+++ create-react-express/client/src/Components/Stylesheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function Stylesheet() {
+    return (
+        <div>
+            <h2 className= 'primary'>Google Books Search</h2>
+        </div>
+    )
+}
+
+export default Stylesheet
